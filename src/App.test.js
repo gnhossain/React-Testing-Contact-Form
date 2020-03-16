75b8cce04d8bcb9f5845fb46f197d4ecf9f061e9
gul hossain <spicymangojungle@gmail.com>
2020-03-15 23:55:13 -0700

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,31 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+test("simple test by text", () => {
+  const { getByText } = render(<App />);
+ 
+  const fName = getByText(/first name/i);
+  const lName = getByText(/last name/i);
+ 
+  expect(fName).toBeInTheDocument();
+  expect(fName).toBeTruthy()
+
+  expect(lName).toBeInTheDocument();
+  expect(lName).toBeTruthy()
+ });
+
+//   test("email type", () => {
+//   const { getByDisplayValue } = render(<App/>);
+ 
+//   const etype = getByDisplayValue(email);
+ 
+//   expect(etype).toHaveFormValue();
+//  });
+
+// test('inputs are rendered-firstname, last, email, and message ', () => {
+//     const {getByLabelText} =render(<App/>);
+//     getByLabelText(/first name/i);
+//     getByLabelText(/last name/i);
+//     getByLabelText(/email/i);
+//     getByLabelText(/message/i);
+// })
